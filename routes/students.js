d2d386e43a329e7e41255863a1bb7e004c489e8b
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-06-12 10:46:49 -0700

    purchases endpoint added

--- routes/students.js
+++ routes/students.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getStudentInfo } = require('../queries/studentsQueries');
+const { getStudentInfo,
+  getStudentPasses,
+  createPassPurchase,
+  getStudentPassesCount } = require('../queries/studentsQueries');
 
 module.exports = (db) => {
 
@@ -20,6 +23,66 @@ module.exports = (db) => {
       }
     }
     catch (error) {
+      res.send({ status: 'failed' })
+      throw error
+    }
+  });
+
+  router.post('/:student_id/passes', async (req, res) => {
+    try {
+      const type = req.body.type
+      const id = req.params.student_id
+
+      function getExpiration() {
+        let expiration;
+        const dateToday = new Date()
+        let day = dateToday.getDate()
+        let month = dateToday.getMonth() + 1
+        let year = dateToday.getFullYear()
+        if (month !== 12) {
+          expiration = [month + 1, day, year]
+        } else {
+          expiration = [1, day, year + 1]
+        }
+        return expiration
+      }
+
+      let limit;
+      switch (type) {
+        case 'single':
+          limit = 1;
+          break;
+        case '5-pack':
+          limit = 5;
+          break;
+        case '25-pack':
+          limit = 25
+          break;
+        case 'monthly':
+          limit = getExpiration();
+          break;
+        default:
+          limit = 1;
+      }
+
+      let responseObj = await db.query(getStudentPassesCount, [id, limit])
+      const initPassCount = responseObj.rows[0].count
+      console.log(initPassCount);
+
+      await db.query(createPassPurchase, [type, id, limit])
+
+      responseObj = await db.query(getStudentPassesCount, [id, limit])
+      const updatedPassCount = responseObj.rows[0].count
+      console.log(updatedPassCount);
+
+      if (Number(updatedPassCount) === Number(initPassCount) + 1) {
+        res.send({ status: 'success' })
+      } else {
+        res.send({ status: 'failed' })
+      }
+    }
+    catch (error) {
+      res.send({ status: 'failed error' })
       throw error
     }
   });
